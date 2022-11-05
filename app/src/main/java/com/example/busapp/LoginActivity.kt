0a6057d43d13e20b475e66795eb075d2acfd0720
Alex Arhuata T <alearhuatat@upt.pe>
2022-11-05 14:10:33 -0500

    login background and google button

--- app/src/main/java/com/example/busapp/LoginActivity.kt
+++ app/src/main/java/com/example/busapp/LoginActivity.kt
@@ -8,11 +8,20 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.busapp.ui.theme.BusAppTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -43,8 +52,40 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Button(onClick = { signIn() }) {
-                        Text(text = "Iniciar Sesion")
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .blur(radius = 30.dp),
+                        painter = painterResource(R.drawable.background_bus),
+                        contentDescription = "background_image",
+                        contentScale = ContentScale.FillBounds
+                    )
+                    Surface(color = Color.Black.copy(alpha = 0.6f),
+                        modifier = Modifier.fillMaxSize()) {
+                        Column(
+
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center) {
+                            Button(
+                                onClick = {
+                                    signIn()
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 16.dp, end = 16.dp),
+                                shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = Color.White,
+                                    contentColor = Color.Black
+                                )
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.ic_google_logo),
+                                    contentDescription = ""
+                                )
+                                Text(text = "Inicie Sesión con una cuenta Google UPT ", modifier = Modifier.padding(6.dp))
+                            }
+                        }
                     }
                 }
             }
